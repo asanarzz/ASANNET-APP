e372cb13132c66f150a6e10ef0ffcd47bd0094fa
asanarzz <asanarz2020@gmail.com>
2026-09-13 14:35:00 +0330

    Add files via upload

--- app/src/main/java/com/kafinet/asannet/VideoDetailActivity.kt
+++ app/src/main/java/com/kafinet/asannet/VideoDetailActivity.kt
@@ -63,6 +63,7 @@ class VideoDetailActivity : AppCompatActivity() {
         val density = resources.displayMetrics.density
         val button = Button(this)
         button.text = label.ifBlank { getString(R.string.btn_download) }
+        button.textDirection = android.view.View.TEXT_DIRECTION_LTR
         button.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.primary)))
         button.setTextColor(ContextCompat.getColor(this, R.color.white))
         button.textSize = 15f
