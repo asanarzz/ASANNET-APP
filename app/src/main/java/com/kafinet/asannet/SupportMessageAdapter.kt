e683c177fd76040bf425471ff362653debe7b00d
asanarzz <asanarz2020@gmail.com>
2026-09-19 14:12:57 +0330

    Add files via upload

--- app/src/main/java/com/kafinet/asannet/SupportMessageAdapter.kt
+++ app/src/main/java/com/kafinet/asannet/SupportMessageAdapter.kt
@@ -2,6 +2,7 @@ package com.kafinet.asannet
 
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
